bcb056818b5d62bd7a8640f101717c94cb99ca2d
joachimharms <joachimharms77@gmail.com>
2018-02-18 16:38:19 +0100

    ToDo List doesn't show up

--- Exercise 1/Start/MyTunes.Shared/SongLoader.cs
+++ Exercise 1/Start/MyTunes.Shared/SongLoader.cs
@@ -7,22 +7,27 @@ using System.Reflection;
 
 namespace MyTunes
 {
-	public static class SongLoader
-	{
-		const string Filename = "songs.json";
+    public static class SongLoader
+    {
+        const string Filename = "songs.json";
 
-		public static async Task<IEnumerable<Song>> Load()
-		{
-			using (var reader = new StreamReader(OpenData())) {
-				return JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
-			}
-		}
+        public static async Task<IEnumerable<Song>> Load()
+        {
+            using (var reader = new StreamReader(OpenData()))
+            {
+                return JsonConvert.DeserializeObject<List<Song>>(await reader.ReadToEndAsync());
+            }
+        }
 
-		private static Stream OpenData()
-		{
-			// TODO: add code to open file here.
-			return null;
-		}
-	}
+        private static Stream OpenData()
+        {
+            // TODO: add code to open file here.
+            #if __ANDROID__
+		        return Android.App.Application.Context.Assets.Open(Filename);
+            #else
+                return null;
+            #endif
+        }
+    }
 }
 
